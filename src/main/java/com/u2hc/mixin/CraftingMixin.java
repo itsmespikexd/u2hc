130df71f71e336db3940d12badc4df6f94bfb4d6
Spike <legalspike2008@gmail.com>
2026-07-29 21:35:04 +0700

    fix utility crafting disabled rule glitching out sometimes

--- src/main/java/com/u2hc/mixin/CraftingMixin.java
+++ src/main/java/com/u2hc/mixin/CraftingMixin.java
@@ -1,41 +1,25 @@
 package com.u2hc.mixin;
 
 import com.u2hc.U2HCMod;
-import net.minecraft.world.Container;
-import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.CraftingMenu;
-import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.inventory.ResultContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.level.block.Blocks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin({CraftingMenu.class, InventoryMenu.class})
-public abstract class CraftingMixin {
+@Mixin(ResultContainer.class)
+public class CraftingMixin {
 
-    @Inject(method = "slotsChanged", at = @At("TAIL"))
-    private void rule20_blockUtilityCrafting(Container container, CallbackInfo ci) {
-        AbstractContainerMenu menu = (AbstractContainerMenu) (Object) this;
-
+    @ModifyVariable(method = "setItem", at = @At("HEAD"), argsOnly = true)
+    private ItemStack rule20_stopUtilityCrafting(ItemStack stack) {
         if (U2HCMod.ACTIVE_CONFIG != null && U2HCMod.ACTIVE_CONFIG.blockUtilityCrafting) {
-            menu.slots.stream()
-                    .filter(slot -> slot.container instanceof ResultContainer)
-                    .findFirst()
-                    .ifPresent(slot -> {
-                        ItemStack result = slot.getItem();
-                        if (!result.isEmpty()) {
-                            if (result.is(Items.FURNACE) ||
-                                result.is(Items.ENCHANTING_TABLE) ||
-                                result.is(Items.BREWING_STAND)) {
-
-                                slot.set(ItemStack.EMPTY);
-                            }
-                        }
-                    });
+            if (stack.is(Items.FURNACE) ||
+                stack.is(Items.ENCHANTING_TABLE) ||
+                stack.is(Items.BREWING_STAND)) {
+                return ItemStack.EMPTY;
+            }
         }
+        return stack;
     }
 }
